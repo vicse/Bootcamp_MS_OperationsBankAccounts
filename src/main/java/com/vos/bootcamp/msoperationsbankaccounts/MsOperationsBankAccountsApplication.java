f7296eddcfb17302ee9795e3bf5d83eacc9eb45d
vicse <vicse.ore@tecsup.edu.pe>
2020-04-02 22:35:34 -0500

    Eureka Client and Config Client configurated

--- src/main/java/com/vos/bootcamp/msoperationsbankaccounts/MsOperationsBankAccountsApplication.java
+++ src/main/java/com/vos/bootcamp/msoperationsbankaccounts/MsOperationsBankAccountsApplication.java
@@ -2,7 +2,9 @@ package com.vos.bootcamp.msoperationsbankaccounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsOperationsBankAccountsApplication {
 

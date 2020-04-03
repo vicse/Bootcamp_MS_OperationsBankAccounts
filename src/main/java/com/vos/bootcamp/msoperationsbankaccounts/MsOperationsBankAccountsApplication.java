2e95d58958fb2a531cf38ec63a2a6946256e243e
vicse <vicse.ore@tecsup.edu.pe>
2020-04-02 20:33:58 -0500

    Project Structure implemented

---
+++ src/main/java/com/vos/bootcamp/msoperationsbankaccounts/MsOperationsBankAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.vos.bootcamp.msoperationsbankaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOperationsBankAccountsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsOperationsBankAccountsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/vos/bootcamp/msoperationsbankaccounts/MsOperationsBankAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vos.bootcamp.msoperationsbankaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOperationsBankAccountsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
